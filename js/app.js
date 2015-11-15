2e85e1597a73e49ee583d38a67f6f544197d760e
Marisa Nanakul <mnanakul@uw.edu>
2015-11-14 20:28:09 -0800

    fixed js merge conflict

--- js/app.js
+++ js/app.js
@@ -3,11 +3,6 @@
 angular.module('SocialIce', [])
 	.controller('signupCtrl', ['$scope', function($scope) {
 
-<<<<<<< HEAD
-		var d = Date.parse($scope.birthdate);
-		console.log(d)
-		
-=======
 		$scope.date = function() {
 			var d = Date.parse($scope.birthdate);
 			if (isNaN(d)) {
@@ -17,7 +12,6 @@ angular.module('SocialIce', [])
 			//console.log(d)
 		}
 
->>>>>>> 1b2de259d5dfac76b73faa2935d3cf06114d60f3
 		$scope.checkMatch = function() {
 			if ($scope.password != null && $scope.passwordConfirm != null && $scope.password === $scope.passwordConfirm) {
 				return true;
