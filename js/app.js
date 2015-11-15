14426296f3d32c5379ba52c046df93c47f3151b6
Isabella Spaletta <spaleisa@uw.edu>
2015-11-14 20:15:44 -0800

    updating code

--- js/app.js
+++ js/app.js
@@ -12,11 +12,6 @@ angular.module('SocialIce', [])
 			//console.log(d)
 		}
 
-<<<<<<< HEAD
-}]);
-
-=======
->>>>>>> 33748cb13068d35a2bd95efef5a5db07a5eb03f1
 		$scope.checkMatch = function() {
 		console.log("hello");
 		// if (password == passwordConfirm) {
