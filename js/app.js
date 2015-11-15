6bef7b1e1f6b1380e62769d0f3c0e6e6ebec3f37
Natasha Dietzler <ndietzler@gmail.com>
2015-11-14 18:57:38 -0800

    add controller to html

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('SocialIce', [])
-	.controller('signupCtrl'), ['$scope', function($scope){
+	.controller('signupCtrl', ['$scope', function($scope) {
 
-}];
+		$scope.checkMatch = function() {
+			console.log("hello");
+			// if (password == passwordConfirm) {
+			// 	console.log("true");
+			// 	return true;
+			// } else {
+			// 	console.log("false");
+			// 	return false;
+			// }
+		}
+
+}]);
