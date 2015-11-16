369227bff6ae8ca2434ce2e1ae0943de7564c5cb
Natasha Dietzler <ndietzler@gmail.com>
2015-11-15 19:15:37 -0800

    resolve #16 create password tests

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -1,38 +1,91 @@
 'use strict';
 
-// describe('To-watch list search page', function(){
+describe('To-watch list search page', function(){
 
-//     //this is a test
-//     it('should have the right title', function(){
-//         browser.get('http://localhost:8000');
+    // //this is a test
+    // it('should have the right title', function(){
+    //     browser.get('http://localhost:8000');
 
-//         //browser.getTitle(); //do nothing with yet
+    //     //browser.getTitle(); //do nothing with yet
 
-//         expect( browser.getTitle() ).toEqual('Watch List');
+    //     expect( browser.getTitle() ).toEqual('Watch List');
 
-//         browser.getTitle().then(function(response){
-//             console.log(response);
-//         });
+    //     browser.getTitle().then(function(response){
+    //         console.log(response);
+    //     });
 
-//     });
+    // });
 
-//     //another test!
-//     it('it should open a modal search window', function(){
-//       browser.get('http://localhost:8000/#/watchlist');
+    // //another test!
+    // it('it should open a modal search window', function(){
+    //   browser.get('http://localhost:8000/#/watchlist');
 
-//       var searchBar = element(by.model('searchQuery'));
-//       searchBar.sendKeys('shawshank');
+    //   var searchBar = element(by.model('searchQuery'));
+    //   searchBar.sendKeys('shawshank');
 
-//       browser.pause();
+    //   browser.pause();
 
-//       var button = element(by.id('searchButton'));
-//       button.click();
+    //   var button = element(by.id('searchButton'));
+    //   button.click();
 
-//       var modalBody = element(by.css('.modal-body'));
-//       expect( modalBody.isPresent() ).toEqual(true);
+    //   var modalBody = element(by.css('.modal-body'));
+    //   expect( modalBody.isPresent() ).toEqual(true);
 
 
 
-//     });
+    // });
 
-// })
+    it('should test that the user has entered text into both fields', function(){
+
+      browser.get('http://localhost:8000');
+
+      var element1 = element(by.model('password'));
+      var element2 = element(by.model('passwordConfirm'));
+      
+      element1.sendKeys('a');
+      element2.sendKeys('a');
+
+      expect( element1.getText() != null && element2.getText() != null ).toEqual(true);
+
+    });
+
+    it('should test that the user has NOT entered text into both fields', function(){
+
+      browser.get('http://localhost:8000');
+
+      var element1 = element(by.model('password'));
+      var element2 = element(by.model('passwordConfirm'));
+
+      expect( element1.getText() && element2.getText() ).toEqual('');
+
+    });
+
+    it('should test that the password and the confirmation that the user entered match', function(){
+
+      browser.get('http://localhost:8000');
+
+      var element1 = element(by.model('password'));
+      var element2 = element(by.model('passwordConfirm'));
+
+      element1.sendKeys('a');
+      element2.sendKeys('a');
+
+      expect( element1.getText() ).toEqual( element2.getText() );
+
+    });
+
+    it('should test that the password and the confirmation do not match', function(){
+
+      browser.get('http://localhost:8000');
+
+      var element1 = element(by.model('password'));
+      var element2 = element(by.model('passwordConfirm'));
+
+      element1.sendKeys('a');
+      element2.sendKeys('b');
+
+      expect( element1.getText() !== element2.getText() ).toEqual(true);
+
+    });
+
+})
